71604ef1d6c0a59f138db6dc0f9bb80b044cb1a3
Faheem <faheempktl@gmail.com>
2026-01-10 23:14:22 +0530

    Submission table created

---
+++ LMS-sb/src/main/java/com/example/LMS_sb/models/Submission.java
@@ -0,0 +1,53 @@
+package com.example.LMS_sb.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "submissions",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"assignment_id", "student_id"}
+        )
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Submission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file_url", nullable = false)
+    private String fileUrl;
+
+    @Column
+    private Integer grade; // nullable until graded
+
+    @Column
+    private String feedback;
+
+    @Column(name = "submitted_at", updatable = false)
+    private LocalDateTime submittedAt;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignment_id", nullable = false)
+    private Assignment assignment;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @PrePersist
+    public void onCreate() {
+        this.submittedAt = LocalDateTime.now();
+    }
+}
