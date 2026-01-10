5f9d4b309c54caa6a38628e1568b40f6ffbadb61
Faheem <faheempktl@gmail.com>
2026-01-10 22:10:38 +0530

    User model created

--- LMS-sb/src/main/java/com/example/LMS_sb/models/User.java
+++ LMS-sb/src/main/java/com/example/LMS_sb/models/User.java
@@ -1,11 +1,39 @@
 package com.example.LMS_sb.models;
 
 
-import jakarta.persistence.Table;
-import lombok.Data;
+import com.example.LMS_sb.models.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
 
-@Table(name = "user")
-@Data
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
-    pri
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Email
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "password",nullable = false)
+    private  String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role" ,nullable = false)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
 }
